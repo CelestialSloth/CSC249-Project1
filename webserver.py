cfd69e65e0763d2cb2c788196af59455f0eaab75
CelestialSloth <52363369+CelestialSloth@users.noreply.github.com>
2022-10-13 09:58:29 -0400

    Attempted threading without threading library
    Not working

--- webserver.py
+++ webserver.py
@@ -2,75 +2,7 @@
 import io
 import tempfile
 from socket import *
-from _thread import *
-import threading
 import sys # In order to terminate the program
-
-#from https://www.geeksforgeeks.org/socket-programming-multi-threading-python/
-print_lock = threading.Lock()
-
-#Method adapted from https://www.geeksforgeeks.org/socket-programming-multi-threading-python/
-#Primarily copy/paste from code in phase 1 of this assignment
-def threaded(connectionSocket):
-    while True:
-        try:
-            message = connectionSocket.recv(1024) # Receive the request message from the client
-            if not message:
-                print_lock.release()
-                break
-
-            # Extract the path of the requested object from the message
-            # The path is the second part of HTTP header, identified by [1]
-            #The program would occasionally throw an error here when a machine tried to access
-            #something that didn't exist, ie "HelloWorl.html". The current program only catches
-            #IOErrors, so I set it to throw one when there's an IndexError.
-            try:
-                filename = message.split()[1]
-            except IndexError:
-                raise IOError
-
-            # Because the extracted path of the HTTP request includes
-            # a character '\', we read the path from the second character
-            f = open(filename[1:])
-
-            outputdata = f.read()  # Store the entire contents of the requested file in a temporary buffer
-            f.close()
-
-            # Send one HTTP header line into socket
-            # Code found in this stack overflow post: https://stackoverflow.com/questions/8315209/sending-http-headers-with-python
-            # TA explained that I needed to add .encode()
-            connectionSocket.send('HTTP/1.0 200 OK\r\n'.encode())
-            #connectionSocket.send("Content-Type: text/html\r\n\r\n".encode())
-
-            # Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send("\r\n".encode())
-
-            connectionSocket.close()
-
-        except IOError:
-
-            # Send response message for file not found
-            # The following two lines were found in this Stack Overflow post: https://stackoverflow.com/questions/41852380/how-to-abort-a-python-script-and-return-a-404-error
-            connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
-            #connectionSocket.send('Content-Type: text/html\r\n\r\n'.encode())
-
-            #encode and send the 404 error html file instead
-            f = open("404Error.html")
-            outputdata = f.read()
-            f.close()
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send("\r\n".encode())
-
-            # Close client socket
-            connectionSocket.close()
-
-        except Exception:
-            print("something else went wrong")
-
-    #connectionSocket.close()
 
 
 def Main():
@@ -80,24 +12,82 @@ def Main():
       # Assign a port number
       # Bind the socket to server address and server port
       # Tell the socket to listen to at most 1 connection at a time
-    PORT = 8080 #we were told that 8080 was a standard port
+    PORT = 8081 #we were told that 8080 was a standard port
     SERVER = gethostbyname(gethostname())  #Diana showed us this code
 
     serverSocket.bind((SERVER, PORT))
     serverSocket.listen(5)
+
+    connections = []
 
     while True:
 
         # Establish the connection
         print('Ready to serve...')
 
-        connectionSocket, addr = serverSocket.accept() # Set up a new connection from the client
+        cSock, addr = serverSocket.accept()  # Set up a new connection from the client
 
+        connections.append(cSock)
+
+        for connectionSocket in connections:
+
+            try:
+                message = connectionSocket.recv(1024)  # Receive the request message from the client
+                if not message:
+                    break
+
+                # Extract the path of the requested object from the message
+                # The path is the second part of HTTP header, identified by [1]
+                #The program would occasionally throw an error here when a machine tried to access
+                #something that didn't exist, ie "HelloWorl.html". The current program only catches
+                #IOErrors, so I set it to throw one when there's an IndexError.
+                try:
+                    filename = message.split()[1]
+                except IndexError:
+                    raise IOError
+
+                # Because the extracted path of the HTTP request includes
+                # a character '\', we read the path from the second character
+                f = open(filename[1:])
+
+                outputdata = f.read()  # Store the entire contents of the requested file in a temporary buffer
+                f.close()
+
+                # Send one HTTP header line into socket
+                # Code found in this stack overflow post: https://stackoverflow.com/questions/8315209/sending-http-headers-with-python
+                # TA explained that I needed to add .encode()
+                connectionSocket.send('HTTP/1.0 200 OK\r\n'.encode())
+                connectionSocket.send("Content-Type: text/html\r\n\r\n".encode())
+
+                # Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+                connectionSocket.send("\r\n".encode())
+
+                connectionSocket.close()
+
+            except IOError:
+                # Send response message for file not found
+                # The following two lines were found in this Stack Overflow post: https://stackoverflow.com/questions/41852380/how-to-abort-a-python-script-and-return-a-404-error
+                connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
+                connectionSocket.send('Content-Type: text/html\r\n\r\n'.encode())
+
+                #encode and send the 404 error html file instead
+                f = open("404Error.html")
+                outputdata = f.read()
+                f.close()
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+                connectionSocket.send("\r\n".encode())
+
+                # Close client socket
+                connectionSocket.close()
+
+            except Exception:
+                print("something else went wrong")
 
 
         #code from https://www.geeksforgeeks.org/socket-programming-multi-threading-python/
-        print_lock.acquire()
-        start_new_thread(threaded, (connectionSocket,))
 
     serverSocket.close()
     sys.exit()  #Terminate the program after sending the corresponding data
