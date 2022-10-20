0e203c2dd1dcf116b937b2c8a14e7d0487b8f9bd
CelestialSloth <52363369+CelestialSloth@users.noreply.github.com>
2022-10-20 14:19:26 -0400

    Client can take command line arguments

--- client.py
+++ client.py
@@ -1,13 +1,26 @@
 import requests
+import sys
 
-def main():
-    print("main")
-    server_host = '127.0.0.1'
-    server_port = '8080'
-    filename = 'HelloWorld.html'
-    r = requests.get('http://'+server_host + ':' + server_port + '/' + filename)
-    #r = requests.get('https://httpbin.org/')
+def main(server_host, server_port, filename):
+
+
+    website = 'http://'+server_host + ':' + server_port + '/' + filename
+
+    #Found method in https://www.tutorialspoint.com/python_network_programming/python_http_client.htm
+    r = requests.get(website)
+
     print(r.text)
 
 if __name__=='__main__':
-    main()
+    #found in https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+    arguments = sys.argv
+    if(len(arguments) < 4):
+        server_host = '127.0.0.1'
+        server_port = '8080'
+        filename = 'HelloWorld.html'
+
+    else:
+        server_host = arguments[1]
+        server_port = arguments[2]
+        filename = arguments[3]
+    main(server_host, server_port, filename)
